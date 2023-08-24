d9188c2f739af450668ed8fb050facd9b8f596f2
Sasoste <121641034+Sasoste@users.noreply.github.com>
2023-08-24 15:30:48 +0200

    retrait apostrophe

--- components/About.jsx
+++ components/About.jsx
@@ -1,23 +1,23 @@
 import styles from "../styles/About.module.scss";
-import Head from "next/head";
-
 
 function About() {
   return (
     <div id="about" className={`${styles.container} section`}>
       <section className={styles.about}>
         <p className={styles.accroche}>À Propos</p>
-        <h2>
-          Qui je suis 
-        </h2>
+        <h2>Qui je suis</h2>
         <p>
           Je suis un développeur fullstack en formation. <br />
-          Je viens de commencer le développement web, en ayant une expérience passée avec Java. <br />
-          Je suis passionné de sport, développement personnel et finance, j'aime beaucoup apprendre de nouvelles choses. <br />
+          Je viens de commencer le développement web, en ayant une expérience
+          passée avec Java. <br />
+          Je suis passionné de sport, développement personnel et finance,
+          j&apos;aime beaucoup apprendre de nouvelles choses. <br />
           <br />
-          Suite à l'essor des nouvelles technologies, j'ai voulu apprendre le développement web.<br />
-          J'ai donc suivi le parcours de développeur web d'OpenClassroom et voila le résultat de cette formation !<br />
-        
+          Suite à l&apos;essor des nouvelles technologies, j&apos;ai voulu
+          apprendre le développement web.
+          <br />
+          J&apos;ai donc suivi le parcours de développeur web
+          d&apos;OpenClassroom et voila le résultat de cette formation !<br />
         </p>
       </section>
     </div>
